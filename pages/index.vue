4f36c2556be4afb561c6349c02b8ed9df2e8730e
jackmayhew <jacksonmayhew9@gmail.com>
2024-10-31 19:19:20 -0400

    hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
   <main>
     <Hero :isFromMobileMenu="isFromMobileMenu" />
-    <div class="mt-6">
-      <NuxtImg class="hero__image w-full rounded-3xl" src="/hero.gif" rel="preload" />
-    </div>
     <Work />
   </main>
 </template>
