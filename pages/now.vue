30e50f4da1c7996713e8c5bb56e78b6d4482d101
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-01 13:26:55 -0400

    font-weight

--- pages/now.vue
+++ pages/now.vue
@@ -7,7 +7,7 @@
           <div
             class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute sm:before:left-0 before:h-full before:px-px before:bg-neutral-300 dark:before:bg-gray-700 before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute sm:after:left-0 after:w-3 after:h-3 after:bg-neutral-300 dark:after:bg-gray-700 after:mt-[-4.5px] after:border-8 after:box-content after:border-light-bg dark:after:border-dark-bg after:rounded-full after:-translate-x-1/2 after:translate-y-1.5">
             <div class="pl-6 sm:pl-8 text-xl font-bold text-gray-500">
-              <time :datetime="event.formateDate">{{ event.displayDate }}</time>
+              <time :datetime="event.formateDate" class="font-normal">{{ event.displayDate }}</time>
             </div>
           </div>
           <div class="pl-6 sm:pl-8 text-lg">
