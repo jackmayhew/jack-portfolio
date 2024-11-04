54bcc24a9702832a54ff8fbe37c4d8d81ce43eff
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-04 13:12:43 -0500

    add og and twitter meta imgs

--- components/meta/headConfig.js
+++ components/meta/headConfig.js
@@ -13,27 +13,28 @@ export function setupHead(prevPage) {
       { name: "description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop. In case anyone is wondering, I use arch linux." },
       { charset: "UTF-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1.0" },
-      // Open Graph / Facebook
+      // open graph / facebook
       { property: "og:type", content: "website" },
       { property: "og:url", content: "https://jackmayhew.com" },
       { property: "og:title", content: "Jack" },
       { property: "og:description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop. In case anyone is wondering, I use arch linux." },
-      { property: "og:image", content: "/path-to-your-social-image.jpg" },
-      // Twitter
+      { property: "og:image", content: "/og.png" },
+      // twitter
       { property: "twitter:card", content: "summary_large_image" },
       { property: "twitter:url", content: "https://jackmayhew.com" },
       { property: "twitter:title", content: "Jack" },
       { property: "twitter:description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop. In case anyone is wondering, I use arch linux." },
-      { property: "twitter:image", content: "/path-to-your-social-image.jpg" }
+      { property: "twitter:image", content: "/twitter.png" }
     ],
     link: [
+      // favicons
       { rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png", sizes: "96x96" },
       { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
       { rel: "shortcut icon", href: "/favicon/favicon.ico" },
       { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
       { rel: "manifest", href: "/favicon/site.webmanifest" },
       { rel: "canonical", href: "https://jackmayhew.com" },
-      // Preloads
+      // preloads
       { rel: "preload", href: "/hero.webp", as: "image" },
       { rel: "preload", href: "/fonts/StabilGrotesk-Regular.woff2", as: "font", type: "font/woff2", crossorigin: "anonymous" },
       { rel: "preload", href: "/fonts/StabilGrotesk-Medium.woff2", as: "font", type: "font/woff2", crossorigin: "anonymous" },
