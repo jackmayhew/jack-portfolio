cde37fb8e7d69ecbd5126d7f151fb739bb683b12
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-14 11:19:47 -0500

    meta stuff

--- components/meta/headConfig.js
+++ components/meta/headConfig.js
@@ -14,21 +14,22 @@ export function setupHead(prevPage) {
     //   },
     // ],
     meta: [
-      { name: "description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop. In case anyone is wondering, I use arch linux." },
+      { name: "description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop." },
       { charset: "UTF-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1.0" },
       // open graph / facebook
       { property: "og:type", content: "website" },
+      { property: "og:site_name", content: "Jack Mayhew" },
       { property: "og:url", content: "https://jackmayhew.com" },
       { property: "og:title", content: "Jack Mayhew" },
-      { property: "og:description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop. In case anyone is wondering, I use arch linux." },
-      { property: "og:image", content: "/img/og.png" },
+      { property: "og:description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop." },
+      { property: "og:image", content: "https://jackmayhew.com/img/og.png" },
       // twitter
       { property: "twitter:card", content: "summary_large_image" },
       { property: "twitter:url", content: "https://jackmayhew.com" },
       { property: "twitter:title", content: "Jack Mayhew" },
-      { property: "twitter:description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop. In case anyone is wondering, I use arch linux." },
-      { property: "twitter:image", content: "/img/twitter.png" }
+      { property: "twitter:description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop." },
+      { property: "twitter:image", content: "https://jackmayhew.com/img/twitter.png" }
     ],
     link: [
       // favicons
