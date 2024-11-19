07e7e63890efeb4e93c7eb9054c7b4075a7853c3
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-19 17:00:06 -0500

    update work card

--- components/work/card.vue
+++ components/work/card.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mb-28">
     <NuxtImg class="work-img w-full h-full rounded-2xl opacity-0 invisible bg-white" :src="project.image" :alt="project.title" loading="lazy" width="800" height="510" />
-    <div class="work-text pb-6 sticky bottom-0">
+    <div class="work-text py-6 sticky bottom-0">
       <time class="mt-4 sm:mt-8 text-lg text-gray-400" :datetime="project.date">{{ project.date }}</time>
       <h2 class="mt-2 text-2xl sm:text-4xl font-semibold">{{ project.title }}</h2>
       <p class="my-2 text-xl sm:text-2xl">{{ project.desc }}</p>
-      <a class="flex items-center gap-2 w-fit" :href="project.url" target="_blank" rel="noopener">
+      <a class="flex items-center gap-2 mt-4 w-fit" :href="project.url" target="_blank" rel="noopener">
         <Icon name="tabler:link" size="28" />
         <span class="text-lg font-semibold">Visit</span>
       </a>
