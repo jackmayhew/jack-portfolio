529d8abe4ba85fe5fd7eb07daf333e8e334728cf
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-25 17:08:16 -0500

    menu

--- components/nav/mobileMenu.vue
+++ components/nav/mobileMenu.vue
@@ -92,7 +92,7 @@ function toggleNav() {
             .set(wrapper.value, { height: wrapperHeight, opacity: 0, width: "100%" })
             .set(wrapperInner.value, { y: "-3.5rem", scaleX: 0, width: "3rem", height: "3rem" })
             .set('.menu-overlay', { opacity: 1, duration: .6 })
-            // .set('.nav-item', { opacity: 0, x: 60 })
+            .set('.nav-item', { opacity: 0, x: 60 })
             .set('.footer-bg', { width: "0" })
             .to(wrapper.value, { opacity: 1, width: "100%" })
             .to(wrapperInner.value, {
@@ -107,13 +107,13 @@ function toggleNav() {
                 duration: .6,
                 ease: "expo.inOut",
             }, .3)
-            // .to('.nav-item', {
-            //     opacity: 1,
-            //     x: 0,
-            //     duration: (index) => 1 + index * 0.05,
-            //     stagger: 0.1,
-            //     ease: 'expo.out',
-            // }, '-=0.4')
+            .to('.nav-item', {
+                opacity: 1,
+                x: 0,
+                duration: (index) => 1 + index * 0.05,
+                stagger: 0.1,
+                ease: 'expo.out',
+            }, '-=0.4')
             .to('.footer-bg', {
                 width: "100%",
                 duration: .8,
@@ -197,7 +197,7 @@ ul {
     list-style: none;
     margin-bottom: .5rem;
     font-size: 2.25rem;
-    /* opacity: 0; */
+    opacity: 0;
     /* transform: translateX(60px); */
     font-weight: 600;
 }
@@ -341,7 +341,7 @@ ul {
     background-color: rgba(18, 18, 18, 0.8)
 }
 
-/* .router-link-active:before {
+.router-link-active:before {
     position: absolute;
     top: calc(50% - 3px);
     left: -1rem;
@@ -361,7 +361,7 @@ ul {
 
 .router-link-active:before {
     transform: translate3d(0, 0, 0) scale(1);
-} */
+}
 
 .pointer-events-auto {
     pointer-events: auto !important;
