cb172e4475abf552acb3525814d0383e1e34cd9c
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-25 16:03:44 -0500

    menu

--- components/nav/mobileMenu.vue
+++ components/nav/mobileMenu.vue
@@ -107,28 +107,18 @@ function toggleNav() {
                 duration: .6,
                 ease: "expo.inOut",
             }, .3)
-            // .to('.nav-item', {
-            //     opacity: 1,
-            //     x: 0,
-            //     duration: (index) => 1 + index * 0.05,
-            //     stagger: 0.1,
-            //     ease: 'expo.out',
-            // }, '-=0.4')
             .to('.nav-item', {
                 opacity: 1,
                 x: 0,
-                duration: 1,
+                duration: (index) => 1 + index * 0.05,
+                stagger: 0.1,
                 ease: 'expo.out',
-                onUpdate: () => {
-                    // Ensure button remains clickable during entire animation
-                    document.querySelector('.hamburger-button').style.pointerEvents = 'auto';
-                }
-            }, .6)
-            .to('.footer-bg', {
-                width: "100%",
-                duration: .8,
-                ease: "expo.inOut",
-            }, .3)
+            }, '-=0.4')
+            // .to('.footer-bg', {
+            //     width: "100%",
+            //     duration: .8,
+            //     ease: "expo.inOut",
+            // }, .3)
     }
 
     menuIsOpen.value = !menuIsOpen.value;
