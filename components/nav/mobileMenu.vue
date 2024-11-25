e1f1f248e7b19c6c31446f7ae1db12d69d42b1f5
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-25 14:33:39 -0500

    mob menu

--- components/nav/mobileMenu.vue
+++ components/nav/mobileMenu.vue
@@ -114,6 +114,13 @@ function toggleNav() {
             //     stagger: 0.1,
             //     ease: 'expo.out',
             // }, '-=0.4')
+            .to('.nav-item', {
+                opacity: 1,
+                x: 0,
+                duration: 1,
+                stagger: 0.1,
+                ease: 'expo.out',
+            }, '-=0.4')
             .to('.footer-bg', {
                 width: "100%",
                 duration: .8,
