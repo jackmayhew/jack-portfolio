26745b6a4b208e721900cdd174a7cd8b8b206bc7
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-26 14:41:47 -0500

    meta tag theme plugin

--- plugins/color-mode.js
+++ plugins/color-mode.js
@@ -1,17 +1,14 @@
 export default defineNuxtPlugin(() => {
-    const colorMode = useColorMode()
-    const updateThemeColor = () => {
-      const themeColorMeta = document.querySelector('meta[name="theme-color"]')
-      if (themeColorMeta) {
-        const color = colorMode.preference === 'dark' ? '#121212' : '#FDFAF5'
-        themeColorMeta.setAttribute('content', color)
-      } else {
-        console.warn('meta[name="theme-color"] not found')
-      }
+  const colorMode = useColorMode()
+  const updateThemeColor = () => {
+    const themeColorMeta = document.querySelector('meta[name="theme-color"]')
+    if (themeColorMeta) {
+      // Use transparent or a color that matches your background transition
+      themeColorMeta.setAttribute('content', 'transparent')
     }
+  }
 
-    updateThemeColor()
-  
-    watch(() => colorMode.preference, updateThemeColor)
-  })
-  
+  updateThemeColor()
+
+  watch(() => colorMode.preference, updateThemeColor)
+})
