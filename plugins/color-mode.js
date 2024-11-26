55f4271e50338e519a7b5ecc873096d0069ec4c2
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-26 12:44:21 -0500

    color mode plugin

--- plugins/color-mode.js
+++ plugins/color-mode.js
@@ -1,16 +1,17 @@
 export default defineNuxtPlugin(() => {
-  const colorMode = useColorMode();
-  const updateThemeColor = () => {
-    const themeColorMeta = document.querySelector('meta[name="theme-color"]');
-    if (themeColorMeta) {
-      const color = colorMode.preference === "dark" ? "#FDFAF5" : "#121212";
-      themeColorMeta.setAttribute("content", color);
-    } else {
-      console.warn('meta[name="theme-color"] not found');
+    const colorMode = useColorMode()
+    const updateThemeColor = () => {
+      const themeColorMeta = document.querySelector('meta[name="theme-color"]')
+      if (themeColorMeta) {
+        const color = colorMode.preference === 'dark' ? '#121212' : '#FDFAF5'
+        themeColorMeta.setAttribute('content', color)
+      } else {
+        console.warn('meta[name="theme-color"] not found')
+      }
     }
-  };
 
-  updateThemeColor();
-
-  watch(() => colorMode.preference, updateThemeColor);
-});
+    updateThemeColor()
+  
+    watch(() => colorMode.preference, updateThemeColor)
+  })
+  
