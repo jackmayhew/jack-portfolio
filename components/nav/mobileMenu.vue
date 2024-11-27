d9a5997094e155be18946da4f07723a7d1c61726
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-27 16:36:14 -0500

    menu

--- components/nav/mobileMenu.vue
+++ components/nav/mobileMenu.vue
@@ -135,12 +135,12 @@ watch(() => route.path, () => {
     }
 });
 
-onClickOutside(wrapper, event => {
-    // if (event.target.closest('.ignore-click')) return;
-    if (!menuIsOpen.value || menuIsAnimating.value || event.target.closest('.ignore-click')) return;
-    menuIsAnimating.value = false;
-    toggleNav();
-})
+// onClickOutside(wrapper, event => {
+//     // if (event.target.closest('.ignore-click')) return;
+//     if (!menuIsOpen.value || menuIsAnimating.value || event.target.closest('.ignore-click')) return;
+//     menuIsAnimating.value = false;
+//     toggleNav();
+// })
 </script>
 
 <style scoped>
