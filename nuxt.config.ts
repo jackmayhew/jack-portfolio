763301ee3c042f37b0670d1713e02fa266ac96a5
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-27 14:55:39 -0500

    plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     "@nuxtjs/color-mode",
     "nuxt-gtag",
   ],
-  plugins: ['@/plugins/color-mode.js'],
+  // plugins: ['@/plugins/color-mode.js'],
   components: [
     {
       path: "~/components",
