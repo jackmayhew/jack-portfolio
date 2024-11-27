04efc679952a638278fdde0afbaf387c05ae0843
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-27 09:22:51 -0500

    menu

--- components/nav/navbar3.vue
+++ components/nav/navbar3.vue
@@ -1,49 +1,38 @@
 <template>
   <header class="navbar px-6">
     <nav class="py-4 mx-auto relative flex justify-between items-center">
-      <NuxtLink to="/" class="nav-logo text-xl font-semibold z-10">
+      <a to="/" class="nav-logo text-xl font-semibold z-10">
         Jack
-      </NuxtLink>
-      <ul class="flex items-center gap-4 sm:gap-6">
-        <!-- <li v-for="link in navigationLinks" :key="link.path" class="hidden xs:block">
-          <NuxtLink :to="link.path" class="link text-lg z-50">
-            {{ link.name }}
-          </NuxtLink>
-        </li> -->
-        <li class="hidden xs:block">
-          <a href="https://github.com/jackmayhew" target="_blank" rel="noopener noreferrer" class="link text-lg">
-            GitHub
+      </a>
+
+      <ul class="flex items-center gap-4 sm:gap-6 w-fit justify-end">
+        <li class="hidden sm:block">
+          <a href="https://github.com/" target="_blank" rel="noopener noreferrer" class="link text-lg">
+            Navbar
           </a>
         </li>
-        <!-- <li class="w-6 h-6 flex z-10">
-          <ClientOnly>
-            <div :class="colorMode.value === 'dark' ? 'dark' : 'light'" class="flex toggle">
-              <button class="ignore-click" title="Toggle Theme" @click="toggleColorMode">
-                <ThemeToggle />
-              </button>
-            </div>
-          </ClientOnly>
-        </li> -->
-        <li class="flex xs:hidden">
+
+        <li class="flex sm:hidden">
           <MobileMenu />
         </li>
       </ul>
+      
     </nav>
   </header>
 </template>
 
 <script setup>
-const colorMode = useColorMode()
+// const colorMode = useColorMode()
 
-const navigationLinks = [
-  { path: '/about', name: 'About' },
-  { path: '/now', name: 'Now' },
-  { path: '/contact', name: 'Contact' },
-];
+// const navigationLinks = [
+//   { path: '/about', name: 'About' },
+//   { path: '/now', name: 'Now' },
+//   { path: '/contact', name: 'Contact' },
+// ];
 
-const toggleColorMode = () => {
-  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
-};
+// const toggleColorMode = () => {
+//   colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+// };
 </script>
 
 <style scoped>
