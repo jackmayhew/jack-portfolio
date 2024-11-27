c8d6f8a41db21220fe7b56561b84a85a2e1d1468
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-27 16:17:21 -0500

    menu

--- components/work/work.vue
+++ components/work/work.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-24">
+  <div class="mt-24 relative">
     <h2 class="mb-24 text-4xl sm:text-5xl">Selected Work</h2>
     <Card v-for="(project) in projects" :key="project.id" :project="project" />
   </div>
@@ -10,48 +10,27 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { projects } from "~/data/projectCards";
 // gsap.registerPlugin(ScrollTrigger);
-
 // even just gsap.registerPlugin(ScrollTrigger); causes error
 
 onMounted(() => {
-  console.log('pain')
-  setTimeout(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    const imgs = document.querySelectorAll('.work-img');
-    imgs.forEach((img) => {
-      gsap.fromTo(
-        img,
-        { autoAlpha: 0 },
-        {
-          autoAlpha: 1,
-          scrollTrigger: {
-            trigger: img,
-            start: "top 55%",
-            end: "top 10%",
-            scrub: true,
-            toggleActions: "play none none reverse",
-          },
-        }
-      );
-    });
-  }, 10000);
-  // gsap.registerPlugin(ScrollTrigger);
-  // const imgs = document.querySelectorAll('.work-img');
-  // imgs.forEach((img) => {
-  //   gsap.fromTo(
-  //     img,
-  //     { autoAlpha: 0 },
-  //     {
-  //       autoAlpha: 1,
-  //       scrollTrigger: {
-  //         trigger: img,
-  //         start: "top 55%",
-  //         end: "top 10%",
-  //         scrub: true,
-  //         toggleActions: "play none none reverse",
-  //       },
-  //     }
-  //   );
-  // });
+  gsap.registerPlugin(ScrollTrigger);
+  const imgs = document.querySelectorAll('.work-img');
+  imgs.forEach((img) => {
+    gsap.fromTo(
+      img,
+      { autoAlpha: 0 },
+      {
+        autoAlpha: 1,
+        scrollTrigger: {
+          trigger: img,
+          start: "top 55%",
+          end: "top 10%",
+          scrub: true,
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+  });
+  ScrollTrigger.refresh()
 });
 </script>
