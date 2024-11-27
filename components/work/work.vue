40c59f2b092f2bcb4d6b9a5e1a27064744059f70
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-27 15:36:25 -0500

    menu

--- components/work/work.vue
+++ components/work/work.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-24">
     <h2 class="mb-24 text-4xl sm:text-5xl">Selected Work</h2>
-    <!-- <Card v-for="(project) in projects" :key="project.id" :project="project" /> -->
+    <Card v-for="(project) in projects" :key="project.id" :project="project" />
   </div>
 </template>
 
@@ -12,23 +12,23 @@ import { projects } from "~/data/projectCards";
 
 gsap.registerPlugin(ScrollTrigger);
 
-onMounted(() => {
-    const imgs = document.querySelectorAll('.work-img');
-    imgs.forEach((img) => {
-      gsap.fromTo(
-        img,
-        { autoAlpha: 0 },
-        {
-          autoAlpha: 1,
-          scrollTrigger: {
-            trigger: img,
-            start: "top 55%",
-            end: "top 10%",
-            scrub: true,
-            toggleActions: "play none none reverse",
-          },
-        }
-      );
-    });
-});
+// onMounted(() => {
+//     const imgs = document.querySelectorAll('.work-img');
+//     imgs.forEach((img) => {
+//       gsap.fromTo(
+//         img,
+//         { autoAlpha: 0 },
+//         {
+//           autoAlpha: 1,
+//           scrollTrigger: {
+//             trigger: img,
+//             start: "top 55%",
+//             end: "top 10%",
+//             scrub: true,
+//             toggleActions: "play none none reverse",
+//           },
+//         }
+//       );
+//     });
+// });
 </script>
