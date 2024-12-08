df533c3781f72bc67246214946d0a72f4c637695
jackmayhew <jacksonmayhew9@gmail.com>
2024-12-08 18:21:49 -0500

    test page

--- pages/test.vue
+++ pages/test.vue
@@ -10,7 +10,7 @@ definePageMeta({
   layout: "test",
 });
 // WHY THE FUCK DOES THIS BREAK THE MENU ANIMATION
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger);
 </script>
