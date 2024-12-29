7ce0f5461e4b36b752d7abb52782d5116e4fc101
jackmayhew <jacksonmayhew9@gmail.com>
2024-12-29 17:13:23 -0500

    gsap stuff

--- layouts/test.vue
+++ layouts/test.vue
@@ -1,42 +1,5 @@
 <template>
-    <div class="wrapper max-w-screen-md mx-auto">
       <Navbar class="relative max-w-screen-md mx-auto" />
-      <div class="main-content px-6 mt-[6.5rem]">
-        <NuxtPage />
-        <Footer />
-      </div>
-    </div>
+      <NuxtPage />
   </template>
   
-  <script setup>  
- 
-  </script>
-  
-  
-  <style>
-  .page-enter-active {
-    transition: all 0.3s ease-out;
-    z-index: -1;
-  }
-  
-  .page-leave-active {
-    transition: all 0.15s ease-in;
-    z-index: -1;
-  }
-  
-  .page-enter-from {
-    opacity: 0;
-    transform: translateY(10px);
-  }
-  
-  .page-leave-to {
-    opacity: 0;
-    transform: translateY(-10px);
-  }
-  
-  .main-content,
-  .wrapper {
-    min-height: 100vh;
-    height: 100%;
-  }
-  </style>
--- pages/test.vue
+++ pages/test.vue
@@ -9,7 +9,6 @@
 definePageMeta({
   layout: "test",
 });
-// WHY THE FUCK DOES THIS BREAK THE MENU ANIMATION
 // import gsap from "gsap";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 // gsap.registerPlugin(ScrollTrigger);
