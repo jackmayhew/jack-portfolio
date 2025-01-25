6b1d9ebac9a296ee9d286310bc72e7d2ca2724cf
jackmayhew <jacksonmayhew9@gmail.com>
2025-01-25 18:31:57 -0500

    mobile menu close  onClickOutside

--- components/nav/mobileMenu.vue
+++ components/nav/mobileMenu.vue
@@ -148,7 +148,7 @@ onBeforeUnmount(() => {
 
 onClickOutside(wrapper, event => {
     // if (event.target.closest('.ignore-click')) return;
-    if (!menuIsOpen.value || menuIsAnimating.value || event.target.closest('.ignore-click')) return;
+    if (event.target.closest('.ignore-click')) return;
     menuIsAnimating.value = false;
     toggleNav();
 })
