e2feff1c965e06b3dd3cbd1f36c0e663101b49b0
Jack <jacksonmayhew9@gmail.com>
2025-08-06 12:33:51 -0400

    style: update button and form styling

--- components/ui/ButtonLinkExternal.vue
+++ components/ui/ButtonLinkExternal.vue
@@ -18,7 +18,7 @@ withDefaults(defineProps<{
     target="_blank"
     rel="noopener"
     :style="{ width }"
-    class="group relative inline-flex h-12 gap-2 text-lg items-center justify-center overflow-hidden rounded-md border-2 border-neutral-300 dark:border-gray-700 bg-transparent px-6 font-medium text-neutral-600 dark:text-dark-text transition-all [box-shadow:0px_4px_0.5px_#a3a3a3] dark:[box-shadow:0px_4px_0.5px_#374151] hover:translate-y-[2px] hover:shadow-none"
+    class="group relative inline-flex h-12 gap-2 text-lg items-center justify-center overflow-hidden rounded-md border-2 border-neutral-300 dark:border-gray-700 bg-transparent px-6 font-medium text-neutral-600 dark:text-dark-text transition-all [box-shadow:0px_4px_0.5px_#d4d4d4] dark:[box-shadow:0px_4px_0.5px_#374151] hover:translate-y-[2px] hover:shadow-none"
   >
     {{ text }}
     <Icon v-if="iconName" :name="iconName" :size="iconSize" class="flex-shrink-0" />
