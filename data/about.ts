cbaad31ae86da7ae969d3b52be890190fbe049f0
Jack <jacksonmayhew9@gmail.com>
2025-08-08 17:48:04 -0400

    refactor: update about page templating and data

--- data/about.ts
+++ data/about.ts
@@ -1,22 +1,27 @@
+// considered using a markdown file, but felt overkill. sticking with js object for now
 export const about = [
   {
-    text: `I started coding in 2019, building websites for friends and family. Nothing fancy, but it was enough to get me hooked.
-    From there, I got to work studying all things web development and working towards landing my first role.`,
+    headline: 'Intro',
+    text: `I started coding in 2019, building websites for friends and family. Nothing fancy, but it was enough to get me hooked. From there, I got to work studying all things web development and working towards landing my first role.`,
   },
   {
-    text: `In 2022, I worked as a full-stack developer at Gain Group in Victoria, BC,
-    building websites for luxury car dealerships and diving deep into the wonderful world of email development.
-    Really enjoyed working with the team and I learned a ton.`,
+    headline: 'Work Experience',
+    text: `In 2022, I worked as a full-stack developer at Gain Group in Victoria, BC, building websites for luxury car dealerships and diving deep into the wonderful world of email development. Really enjoyed working with the team and I learned a ton.`,
   },
   {
-    text: `These days, I split my time between carpentry and freelance web development. 
-    Different tools, but equally satisfying when everything comes together!`,
+    headline: 'Nowadays',
+    text: `These days, I split my time between carpentry and freelance web development. Different tools, but equally satisfying when everything comes together!`,
   },
   {
-    text: `Outside of coding, you can find me playing baseball or soccer, surfing, or playing my bass guitar. 
-    I'm based in Montreal, but Vancouver (my hometown) is always calling me back for visits.`,
+    headline: 'Outside of Coding',
+    text: `Outside of coding, you can find me playing baseball or soccer, surfing, or playing my bass guitar. I'm based in Montreal, but Vancouver (my hometown) is always calling me back for visits.`,
   },
   {
-    text: `If you're around and want to chat <a class="json-link" href="mailto:jackmayhew5@gmail.com">drop me a message!</a>`,
+    headline: 'Get in Touch',
+    text: `If you're around and want to chat `,
+    link: {
+      url: 'mailto:jackmayhew5@gmail.com',
+      text: 'drop me a message!',
+    },
   },
 ]
--- pages/about.vue
+++ pages/about.vue
@@ -16,7 +16,25 @@ useHead({
     </h2>
     <div class="w-full max-w-3xl mx-auto">
       <div class="mt-8 text-lg">
-        <p v-for="(section, index) in about" :key="index" class="mt-5" v-html="section.text" />
+        <div
+          v-for="(section, index) in about"
+          :key="index"
+          class="mt-6"
+        >
+          <h3 class="text-xl dark:text-white underline font-semibold mb-1">
+            {{ section.headline }}
+          </h3>
+          <p>
+            {{ section.text }}
+            <a
+              v-if="section.link"
+              class="json-link"
+              :href="section.link.url"
+            >
+              {{ section.link.text }}
+            </a>
+          </p>
+        </div>
       </div>
     </div>
     <Footer />
